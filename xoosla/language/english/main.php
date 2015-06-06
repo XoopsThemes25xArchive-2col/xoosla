23a65d91a9fff459ea7c69f7bf3aeb25a9f4b05a
mambax7 <mambax7@gmail.com>
2015-06-06 05:43:41 -0400

    Original files

---
+++ xoosla/language/english/main.php
@@ -0,0 +1,27 @@
+<?php
+// Theme language definitions
+// $Id: main.php 2857 2009-02-22 18:12:11Z dugris $
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+/**
+ * ------------------- userbar.html -------------------
+ */
+define( "_THEME_REGISTER", "Register" );
+define( "_THEME_RECOVERPASSWORD", "Recover Password" );
+define( "_THEME_INBOX", "View your messages" );
+define( "_THEME_INBOX_NOTREAD", "message(s) unread" );
+define( "_THEME_NOTIFICATIONS", "Notifications" );
+define( "_THEME_PROFILE", "View your profile" );
+define( "_THEME_LOGOUT", "Click to logout" );
+define( "_THEME_LOGIN", "Click to login" );
+define( "_THEME_HOME", "Site Index" );
+define( "_THEME_NEWS", "News" );
+define( "_THEME_REGISTERFORUM", "Register With Forum" );
+define( "_THEME_DOWNLOADS", "Downloads" );
+define( "_THEME_LINKS", "Links" );
+define( "_THEME_CONTACT", "Contact Us" );
+define( "_THEME_LINKTO", "Link To Us" );
+define( "_THEME_ABOUTUS", "About Us" );
+define( "_THEME_ADMINISTRATION", ":: Administration ::" );
+?>
